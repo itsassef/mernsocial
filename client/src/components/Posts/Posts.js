a9d2ce3dd5e06e449ee139d02c8a8b56fb796bc9
Asif Ali <asif.a@scicom.com.my>
2022-12-23 17:20:19 +0800

    start

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Post from "./Post/Post";
+import { useSelector } from "react-redux";
+
+import useStyles from "./styles";
+
+const Posts = () => {
+    const posts = useSelector((state) => state.posts)
+    console.log(posts);
+    const classes = useStyles();
+
+    
+    return (
+        <>
+        <h1>Posts</h1>
+        <Post />
+        <Post />
+        </>
+    )
+}
+
+
+export default Posts;
